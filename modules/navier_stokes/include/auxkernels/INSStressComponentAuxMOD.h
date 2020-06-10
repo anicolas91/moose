a39998d084aa05a79671ee3aa84115f84d279ed0
anicolas91 <andreanicolas91@gmail.com>
2020-06-09 21:41:59 -0500

    initial clean commit for Stokes with AD snippets

---
+++ modules/navier_stokes/include/auxkernels/INSStressComponentAuxMOD.h
@@ -0,0 +1,36 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "AuxKernel.h"
+
+// Forward Declarations
+
+/**
+ * Computes h_min / |u|
+ */
+class INSStressComponentAuxMOD : public AuxKernel
+{
+public:
+  static InputParameters validParams();
+
+  INSStressComponentAuxMOD(const InputParameters & parameters);
+
+  virtual ~INSStressComponentAuxMOD() {}
+
+protected:
+  virtual Real computeValue();
+
+  // Velocity gradients
+  const VariableGradient & _grad_velocity;
+  const VariableValue & _pressure;
+  const unsigned _comp;
+  const MaterialProperty<Real> & _mu; //converted from AD
+};
